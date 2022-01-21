97e538e244451d4fc5aed23c9cd7606d5a01e18d
Arthur.Ribeiro <63483502+arthurfrp@users.noreply.github.com>
2022-01-21 20:12:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,30 @@ const fs = require('fs');
     const milho = 'Brazilian Corn BRL ' + await page.$eval('span[class="PriceTextUp"]', (span) => {
         return span.innerHTML
     })
+    
+    
+    await page.goto('https://br.advfn.com/investimentos/futuros/soja', {waitUntil: "networkidle2"})
+    const soja = 'Brazilian Corn BRL ' + await page.$eval('span[class="PriceTextUp"]', (span) => {
+        return span.innerHTML
+    })
+    
+    
+    await page.goto('https://br.advfn.com/investimentos/futuros/cafe', {waitUntil: "networkidle2"})
+    const cafe = 'Brazilian Corn BRL ' + await page.$eval('span[class="PriceTextUp"]', (span) => {
+        return span.innerHTML
+    })
 
+    
+    await page.goto('https://br.advfn.com/investimentos/futuros/milho', {waitUntil: "networkidle2"})
+    const milho = 'Brazilian Corn BRL ' + await page.$eval('span[class="PriceTextUp"]', (span) => {
+        return span.innerHTML
+    })
 
     cotacao.push({dolar})
     cotacao.push({gado})
     cotacao.push({milho})
+    cotacao.push({soja})
+    cotacao.push({cafe})
 
 
     console.log(cotacao)
